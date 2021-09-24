962451acacfe3893ead16a87ffd6578a9d3fd391
miranov25 <marian.ivanov@cern.ch>
2021-09-24 16:31:58 +0200

    PWGPP-613 - make modification for sendary tracks see - https://alice.its.cern.ch/jira/browse/PWGPP-613?focusedCommentId=274376&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-274376 Still not suffi cnet  - becuase of the Larm suage in original seeding

--- fastMCKalman/MC/fastSimulationFit.C
+++ fastMCKalman/MC/fastSimulationFit.C
@@ -2,8 +2,8 @@
  *
   .L $fastMCKalman/fastMCKalman/MC/fastSimulation.cxx+g
     .L $fastMCKalman/fastMCKalman/MC/fastSimulationTest.C
-   .L $fastMCKalman/fastMCKalman/MC/fastSimulationFit.C
-  initTreeFast();
+    .L $fastMCKalman/fastMCKalman/MC/fastSimulationFit.C
+ initTreeFast();
  treeFast->SetCacheSize(500000000);
    treeFast->SetAlias("meanptMC","part.getMean(0+0,0)");
     treeFast->SetAlias("meanptMCInv","part.getMean(1+0,0)");
@@ -12,32 +12,13 @@
      treeFast->SetAlias("X0Norm","geom.fLayerX0.fData[0]/0.0001");
      treeFast->SetAlias("sigmaRPhi","geom.fLayerResolRPhi.fData[0]");
    treeFast->SetAlias("sigmaPtRel0","sigmaqPt0*ptMC");
+   treeFast->SetAlias("sigmaPtRel0Mean","sigmaqPt0/meanptMCInv");
+   //
 
     // take into account X0 variation
-  makeFitResolPtLArmV1(treeFast, "sigmaPtRel0", "meanptMCInv:(meandEdxExpInv)*X0Norm:tglMC:Larm:sigmaRPhi", "10/sigmaPtRel0", "", "ptMC>0.1&&Larm>50&&meandEdxExp>0&&meandEdxExp<40",kFALSE);
-
-
-
-  //makeFitResolPtLArmV1(treeFast, "sigmaPtRel0", "meanptMCInv:meandEdxExpInv:tglMC:Larm", "10/sigmaPtRel0", "", "ptMC>0.1&&Larm>10&&meandEdxExp>0&&meandEdxExp<40",kFALSE);
-  //makeFitResolPtLArmV2(treeFast, "sigmaPtRel0", "1/ptMC:meandEdxExpInv:tglMC:Larm:meanptMCInv", "10/sigmaPtRel0", "", "ptMC>0.1&&Larm>10&&meandEdxExp>0&&meandEdxExp<40",kFALSE);
-   //
-  //
-
-  treeFast->Draw("log(sigmaPtRel0/sigmaPtRel0FitLV1):1/ptMC:1/meandEdxExp","ptMC>0.1&&Larm>20&&hasDecay==0&&fPdgCodeMC>0","colz",100000);
-  treeFast->Draw("log(sigmaPtRel0/sigmaPtRel0FitLV2):1/ptMC:1/meandEdxExp","ptMC>0.1&&Larm>20&&hasDecay==0&&fPdgCodeMC>0","colz",100000);
-
-
-
-
-  makeFitResolLArm(treeFast, "sigmaqPt0", "meanptMCInv:1/meandEdxExp:tglMC:Larm", "(10/sigmaqPt0)", "", "ptMC>0.1&&ptMC<10&&Larm>20&&meandEdxExp>0",kFALSE);
-
-  makeFitResolPtLArm(treeFast, "sigmaPtRel0", "meanptMCInv:meandEdxExpInv:tglMC:Larm", "(10/sigmaPtRel0)", "", "ptMC>0.1&&ptMC<10&&Larm>20&&meandEdxExp>0&&hasDecay",kFALSE);
-
- //
-   makeFitResol(treeFast, "sigmaqPt0", "(10/sigmaqPt0)**2", "", "ptMC>0.01&&fMaxLayer>5&&ptMC<10",kFALSE);
-    makeFitResolLArm(treeFast, "sigmaqPt0", "(10/sigmaqPt0)**2", "", "ptMC>0.01&&fMaxLayer>5&&ptMC<10",kFALSE);
-     makeFitResolLArm(treeFast, "sigmaqPt0", "(10/sigmaqPt0)", "", "ptMC>0.01&&ptMC<10&&Larm>20",kFALSE);
-
+    makeFitResolPtLArmV1(treeFast, "sigmaPtRel0", "meanptMCInv:(meandEdxExpInv/X0Norm):tglMC:Larm:sigmaRPhi", "10/sigmaPtRel0", "", "ptMC>0.01&&Larm>30&&sigmaPtRel0<0.5&&!isSecondary",kFALSE);
+    //
+    makeFitResolPtLArmV1(treeFast, "sigmaPtRel0Mean", "meanptMCInv:(meandEdxExpInv/X0Norm):tglMC:Larm:sigmaRPhi", "10/sigmaPtRel0", "", "ptMC>0.01&&Larm>30&& sigmaPtRel0<0.5",kFALSE);
 */
 TF1 *likePseudoHuber = new TF1("likePseudoHuber", AliTMinuitToolkit::PseudoHuberLogLike,-10,10,1);
 TStopwatch timer;
@@ -182,7 +163,7 @@ void makeFitResolPtLArmV1(TTree * treeFast, TString varFit, TString variables, T
   p(1, 0)=1;   p(1, 1)=0.1; p(1, 2)=0.;  p(1, 3)=10.;
   p(2, 0)=2;   p(2, 1)=0.1; p(2, 2)=-2.; p(2, 3)=4.;
   p(3, 0)=-1;  p(3, 1)=0.1; p(3, 2)=-4.; p(3, 3)=4.;
-  p(4, 0)=-4;  p(4, 1)=0.1; p(4, 2)=-5.; p(4, 3)=5.;
+  p(4, 0)=-5;  p(4, 1)=0.1; p(4, 2)=-6.; p(4, 3)=-4.;
   p(5, 0)=-2;  p(5, 1)=0.1; p(5, 2)=-5.; p(5, 3)=5.;
 
   //
@@ -330,17 +311,20 @@ void makeFitResolPtLArmV2(TTree * treeFast, TString varFit, TString variables, T
   timerFunction.Print();
 }
 
-
-
-/// TPC  - chenging lever arm using different points ... for non homogenous detector not working
 void makeFitExample(){
-   makeFitResolLArm(treeFast, "sigmaqPt0", "(10/sigmaqPt0)**2", "", "ptMC>0.01&&fMaxLayer>5&&ptMC<10",kFALSE);
-   //
-   treeFast->SetAlias("sigmaqPt","sqrt(part.fParamIn[].fC[14]+0)");
-  treeFast->SetAlias("Larm","Max$(part.fParamMC[].fX)-Min$(part.fParamMC[Iteration$].fX)");
-  makeFitResolLArm(treeFast, "sigmaqPt", "(10/sigmaqPt)**2", "", "ptMC>0.01&&sigmaqPt>0&&fMaxLayer>6&&ptMC<10&&rndm<0.02",kFALSE);
+  // Status: - to be validated
+  // fit with primary only
+  ///   - bias at low Larm - will be fixed with the seeding
+  ///
+  makeFitResolPtLArmV1(treeFast, "sigmaPtRel0", "meanptMCInv:(meandEdxExpInv/X0Norm):tglMC:Larm:sigmaRPhi", "10/sigmaPtRel0", "", "ptMC>0.01&&Larm>30&&sigmaPtRel0<0.5&&!isSecondary",kFALSE);
+  treeFast->Draw("log(sigmaPtRel0/sigmaPtRel0FitLV1):part.fLengthIn:ptMC","ptMC>1&&part.fLengthIn>40&&fPdgCodeMC>0&&!isSecondary","colz",100000);
   //
+  makeFitResolPtLArmV1(treeFast, "sigmaPtRel0", "meanptMCInv:(meandEdxExpInv/X0Norm):tglMC:part.fLengthIn:sigmaRPhi", "10/sigmaPtRel0", "", "ptMC>0.01&&part.fLengthIn>40&&sigmaPtRel0<0.5&&!isSecondary",kFALSE);
+
 
 }
+
+
+
 
 
