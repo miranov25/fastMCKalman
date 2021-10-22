0f9398acc6b72cd9370e56058e52588ae918124a
miranov25 <marian.ivanov@cern.ch>
2021-10-22 13:38:59 +0200

    committing latest version of test

--- fastMCKalman/MC/fastSimulationTestDraw.C
+++ fastMCKalman/MC/fastSimulationTestDraw.C
@@ -1,15 +1,21 @@
-void drawRungeKuttaTest(){
-  treeUnit0->Draw("log(paramStep.P()/paramRK2.P()):paramRK2.P()/paramIn.P():mass","xOverX0>0.01&&statusStep==1&&paramRK2.P()/paramIn.P()>0.6","colz");
-  gPad->SaveAs("unitTest_paramRK_PLoss.png");
-  treeUnit0->Draw("log(paramStep.P()/param0.P()):paramRK2.P()/paramIn.P():mass","xOverX0>0.01&&statusStep==1&&paramRK2.P()/paramIn.P()>0.6","colz");
-  gPad->SaveAs("unitTest_param0_PLoss.png");
+/*
+  .L $fastMCKalman/fastMCKalman/MC/fastSimulation.cxx+g
+  .L $fastMCKalman/fastMCKalman/MC/fastSimulationTest.C+g
+  .L  $fastMCKalman/fastMCKalman/MC/fastSimulationTestDraw.C
+  initTreeFast()
+ */
+
+void drawTimeLength() {
+  //
+  TCanvas *canvasfitdPdxScaling = new TCanvas("fitdPdxScaling","fitdPdxScaling",1200,400);
+  canvasfitdPdxScaling->SetGrid(1,1);
+  treeFast->Draw("fParamMC[4].fData.fLength/sqrt(1+fParamMC[0].fData.fP[3]**2):fParamMC[0].fData.fP[3]:abs(fParamMC[0].fData.fP[4])","","colz");
+  gPad->SaveAs("fig/trackLengthLayer4_Normed.png");
+  treeFast->Draw("fParamMC[4].fData.fTime/sqrt(1+fParamMC[0].fData.fP[3]**2):fParamMC[0].fData.fP[3]:fParamMC[0].Beta()","","colz");
+  gPad->SaveAs("fig/trackTimeLayer4_Normed.png");
+
+  treeFast->Draw("fParamMC[4].fData.fTime:(fParamMC[0].Beta()*fParamMC[4].fData.fLength/2.99792458e-2):fParamMC[0].Beta()", "fParamMC[4].fData.fX>20", "colz");
 }
-
-void drawRungeKuttaTest() {
-
-  treeUnit0->Draw("(paramStep.fC[9]-param0.fC[9])/(paramStep.fC[9]-paramIn.fC[9]):paramRK2.P()/paramIn.P():mass","xOverX0>0.01&&statusStep==1&&paramRK2.P()/paramIn.P()>0.6","colz");
-  treeUnit0->Draw("(paramStep.fC[9]-paramRK.fC[9])/(paramStep.fC[9]-paramIn.fC[9]):paramRK2.P()/paramIn.P():mass","xOverX0>0.01&&statusStep==1&&paramRK2.P()/paramIn.P()>0.6","colz");
-  treeUnit0->Draw("(paramStep.fC[9]-paramRK2.fC[9])/(paramStep.fC[9]-paramIn.fC[9]):paramRK2.P()/paramIn.P():mass","xOverX0>0.01&&statusStep==1&&paramRK2.P()/paramIn.P()>0.6","colz");
-
-
+void drawdQ() {
+  treeFast->Draw("eLossLog:fParamMC[0].fData.fP[3]:fParamMC[0].Beta()", "fParamMC[].Beta()>0.05&&Iteration$<7", "colz");
 }
