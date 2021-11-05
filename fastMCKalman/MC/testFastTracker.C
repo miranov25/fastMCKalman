f57e1556a565eebd1fc02b48994b5fc065095902
miranov25 <marian.ivanov@cern.ch>
2021-11-05 11:56:57 +0100

    * extend unit test with bz as a parameter

--- fastMCKalman/MC/testFastTracker.C
+++ fastMCKalman/MC/testFastTracker.C
@@ -1,6 +1,7 @@
 
 /*
-     gSystem->Load("AliSkimmedDataAnalysisMaker.so");
+        gSystem->Load("$fastMCKalman/fastMCKalman/aliKalman/test/AliExternalTrackParam.so");
+
   //.L $fastMCKalman/fastMCKalman/MC/fastTracker.h
   .L $fastMCKalman/fastMCKalman/MC/testFastTracker.C++g
   testFasTrackerSimul(100000);
@@ -42,7 +43,7 @@ void testFasTracker(Float_t pt, Float_t bz, Float_t tgl){
 ///    the error estimatro and the data should be within error
 void testFasTrackerSimul(Int_t nPoints) {
   // Double_t pt=0.2,bz=0.5, tgl=0.1;
-  float bz = 0.5;
+
   TTreeSRedirector *pcstream = new TTreeSRedirector("testSeed.root", "recreate");
 
   Double_t pxpypz[3] = {};
@@ -50,6 +51,8 @@ void testFasTrackerSimul(Int_t nPoints) {
 
   Double_t cov[21] = {0};
   for (Int_t i = 0; i < nPoints; i++) {
+    float bzSign=(gRandom->Rndm()>0.5)? 1:-1;
+    float bz = 0.5*bzSign;
     Float_t tgl = gRandom->Rndm();
     Float_t pt = (gRandom->Rndm() + 0.05) * 5;
     Float_t sy = (gRandom->Rndm() + 0.01) * 0.01;
@@ -78,6 +81,7 @@ void testFasTrackerSimul(Int_t nPoints) {
     param.Rotate(paramSeed->GetAlpha());
     param.PropagateTo(paramSeed->GetX(), bz);
     (*pcstream) << "seed" <<
+                "bz="<<bz<<
                 "param.=" << &param <<
                 "paramSeed.=" << paramSeed <<
                 "sy=" << sy <<
@@ -139,24 +143,24 @@ void testFastTrackerEval(){
     TF1 *f1 = new TF1("f1", "[0]*x");
     tree->Draw("(paramSeed.fP[1]-param.fP[1])*(paramSeed.fP[3]-param.fP[3]):paramSeed.fC[7]", "sz>0.0005", "prof");
     tree->GetHistogram()->Fit("f1"); // ~ 1 - looks OK
-    if (TMath::Abs(f1->GetParameter(0) - 1) < 0.05) {
+    if (TMath::Abs(f1->GetParameter(0) - 1) < 0.1) {
       ::Info("testFastTracker C(1,3)", "pullAnalytical - OK");
     } else { ::Error("testFastTracker pull test C(1,2)", "pullAnalytical- FAILED"); };
     //
     tree->Draw("(paramSeed.fP[0]-param.fP[0])*(paramSeed.fP[2]-param.fP[2]):paramSeed.fC[3]", "sz>0.0005", "prof");
     tree->GetHistogram()->Fit("f1"); //  ~1 - looks OK
-    if (TMath::Abs(f1->GetParameter(0) - 1) < 0.05) {
+    if (TMath::Abs(f1->GetParameter(0) - 1) < 0.1) {
       ::Info("testFastTracker C(0,2)", "pullAnalytical - OK");
     } else { ::Error("testFastTracker pull test C(0,2)", "pullAnalytical- FAILED"); };
     //
     tree->Draw("(paramSeed.fP[2]-param.fP[2])*(paramSeed.fP[4]-param.fP[4]):paramSeed.fC[12]", "sz>0.0005", "prof");
     tree->GetHistogram()->Fit("f1"); //  ~1 - looks OK
-    if (TMath::Abs(f1->GetParameter(0) - 1) < 0.05) {
+    if (TMath::Abs(f1->GetParameter(0) - 1) < 0.1) {
       ::Info("testFastTracker C(2,4)", "pullAnalytical - OK");
     } else { ::Error("testFastTracker pull test C(2,4)", "pullAnalytical- FAILED"); };
     tree->Draw("(paramSeed.fP[0]-param.fP[0])*(paramSeed.fP[4]-param.fP[4]):paramSeed.fC[10]", "sz>0.0005", "prof");
     tree->GetHistogram()->Fit("f1"); //  ~  looks OK
-    if (TMath::Abs(f1->GetParameter(0) - 1) < 0.05) {
+    if (TMath::Abs(f1->GetParameter(0) - 1) < 0.1) {
       ::Info("testFastTracker C(0,4)", "pullAnalytical - OK");
     } else { ::Error("testFastTracker pull test C(0,4)", "pullAnalytical- FAILED"); };
   }
