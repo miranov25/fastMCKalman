26be7d5e5211e255bd2854e8004b71c6753f3fd9
miranov25 <marian.ivanov@cern.ch>
2021-11-19 11:29:48 +0100

    unit test to check consistency of the fast simulation

---
+++ fastMCKalman/MC/test_fastSimulation.C
@@ -0,0 +1,11 @@
+/*
+  Here we would like to have tests - which checks internal consistence of the fastSimulation
+  Dedicated debug streaemaers to be used
+
+ */
+
+/// here we would lik to test internal consistency of the seeding
+void testSeeding(){
+   TFile f("fastParticle.root");
+
+}
