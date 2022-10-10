713a40806f7e0acc2372c445864937f07fe886fd
miranov25 <marian.ivanov@cern.ch>
2022-10-10 15:35:15 +0200

    PWGPP-613 - adding skeleton/TODO for pull tests

--- fastMCKalman/MC/test_fastSimulation.C
+++ fastMCKalman/MC/test_fastSimulation.C
@@ -17,10 +17,27 @@ void testInit(){
 }
 /// to check momentum bias in the seeds
 
+/// Test pull of seeds
+/// TODO add automatic alarm - and fix eventual problems
 void testMB(){
   // Here we should make unit tests - it will work only up to some relative energy loss
   // treeSeed->Draw("log(paramSeed.P()/input.P()):log(input2.P()/input.P())","sign0>0");
   treeSeed->Draw("(seed.fP[4]-input.fP[4])/sqrt(seed.fC[14])","","");
   treeSeed->Draw("(seed.fP[4]-input.fP[4])/sqrt(seed.fC[14]):(input2.fP[4]-input.fP[4])/sqrt(seed.fC[14]):fMassMC","input.fP[4]<0&&sign0>0","colz");
+}
+
+void testLooperSmooth(){
+  // checking the X position
+  treeFast->Draw("gyMC:gxMC:(part.fParamMC[].fX==part.fParamMC[Iteration$-2].fX)","Iteration$>2","colz",10);
+}
+
+void testPulls(){
+  /// define aliases  - to put later to the main code
+  /// fit pulls  1D and as function of qPt
+
+  treeFast->Draw("(partFull.fParamIn[0].fP[4]-partFull.fParamMC[0].fP[4])/sqrt(partFull.fParamIn[0].fC[14])","partFull.fStatusMaskIn[0].fData==31");
+  treeFast->Draw("(part.fParamIn[0].fP[4]-part.fParamMC[0].fP[4])/sqrt(part.fParamIn[0].fC[14])","part.fStatusMaskIn[0].fData==31");
+
 
 }
+
