1d861a9a3fb70a700445feaf9a8efb88b7387226
Trenchcoat95 <fedebattisti95@gmail.com>
2022-10-10 16:59:35 +0100

    Last small modification to Pull Test

--- fastMCKalman/MC/test_fastSimulation.C
+++ fastMCKalman/MC/test_fastSimulation.C
@@ -52,58 +52,58 @@ void testPulls(){
 
   //Full Reco
   TH1F *myh = new TH1F("myh","myh",100,-10,10);
-  treeFast->Draw("(partFull.fParamIn[0].fP[4]-partFull.fParamMC[0].fP[4])/sqrt(partFull.fParamIn[0].fC[14])>>myh4","partFull.fStatusMaskIn[0]==31");
+  treeFast->Draw("(partFull.fParamIn[0].fP[4]-partFull.fParamMC[0].fP[4])/sqrt(partFull.fParamIn[0].fC[14])>>myh","partFull.fStatusMaskIn[0].fData==31");
   isOK= abs(1-myh->GetRMS())<4*myh->GetRMSError();
   if (isOK) {::Info("testFastTracker full reco pull test P4","pullAnalytical - OK");
   }else{::Error("testFastTracker full reco pull test P4","pullAnalytical- FAILED");
   }
 
-  treeFast->Draw("(partFull.fParamIn[0].fP[3]-partFull.fParamMC[0].fP[3])/sqrt(partFull.fParamIn[0].fC[9])>>myh3","partFull.fStatusMaskIn[0]==31");
+  treeFast->Draw("(partFull.fParamIn[0].fP[3]-partFull.fParamMC[0].fP[3])/sqrt(partFull.fParamIn[0].fC[9])>>myh","partFull.fStatusMaskIn[0].fData==31");
   isOK= abs(1-myh->GetRMS())<4*myh->GetRMSError();
    if (isOK) {::Info("testFastTracker full reco pull test P3","pullAnalytical - OK");
   }else{::Error("testFastTracker full reco pull test P3","pullAnalytical- FAILED");
   }
   
-  treeFast->Draw("(partFull.fParamIn[0].fP[2]-partFull.fParamMC[0].fP[2])/sqrt(partFull.fParamIn[0].fC[5])>>myh","partFull.fStatusMaskIn[0]==31");
+  treeFast->Draw("(partFull.fParamIn[0].fP[2]-partFull.fParamMC[0].fP[2])/sqrt(partFull.fParamIn[0].fC[5])>>myh","partFull.fStatusMaskIn[0].fData==31");
   isOK= abs(1-myh->GetRMS())<4*myh->GetRMSError();
    if (isOK) {::Info("testFastTracker full reco pull test P2","pullAnalytical - OK");
   }else{::Error("testFastTracker full reco pull test P2","pullAnalytical- FAILED");
   }
-  treeFast->Draw("(partFull.fParamIn[0].fP[1]-partFull.fParamMC[0].fP[1])/sqrt(partFull.fParamIn[0].fC[2])>>myh","partFull.fStatusMaskIn[0]==31");
+  treeFast->Draw("(partFull.fParamIn[0].fP[1]-partFull.fParamMC[0].fP[1])/sqrt(partFull.fParamIn[0].fC[2])>>myh","partFull.fStatusMaskIn[0].fData==31");
   isOK= abs(1-myh->GetRMS())<4*myh->GetRMSError();
    if (isOK) {::Info("testFastTracker full reco pull test P1","pullAnalytical - OK");
   }else{::Error("testFastTracker full reco pull test P1","pullAnalytical- FAILED");
   }
-  treeFast->Draw("(partFull.fParamIn[0].fP[0]-partFull.fParamMC[0].fP[0])/sqrt(partFull.fParamIn[0].fC[0])>>myh","partFull.fStatusMaskIn[0]==31");
+  treeFast->Draw("(partFull.fParamIn[0].fP[0]-partFull.fParamMC[0].fP[0])/sqrt(partFull.fParamIn[0].fC[0])>>myh","partFull.fStatusMaskIn[0].fData==31");
   isOK= abs(1-myh->GetRMS())<4*myh->GetRMSError();
    if (isOK) {::Info("testFastTracker full reco pull test P0","pullAnalytical - OK");
   }else{::Error("testFastTracker full reco pull test P0","pullAnalytical- FAILED");
   }
  
   ////Leg Reco
-  treeFast->Draw("(part.fParamIn[0].fP[4]-part.fParamMC[0].fP[4])/sqrt(part.fParamIn[0].fC[14])>>myh4","part.fStatusMaskIn[0]==31");
+  treeFast->Draw("(part.fParamIn[0].fP[4]-part.fParamMC[0].fP[4])/sqrt(part.fParamIn[0].fC[14])>>myh","part.fStatusMaskIn[0].fData==31");
   isOK= abs(1-myh->GetRMS())<4*myh->GetRMSError();
   if (isOK) {::Info("testFastTracker pull test P4","pullAnalytical - OK");
   }else{::Error("testFastTracker pull test P4","pullAnalytical- FAILED");
   }
 
-  treeFast->Draw("(part.fParamIn[0].fP[3]-part.fParamMC[0].fP[3])/sqrt(part.fParamIn[0].fC[9])>>myh3","part.fStatusMaskIn[0]==31");
+  treeFast->Draw("(part.fParamIn[0].fP[3]-part.fParamMC[0].fP[3])/sqrt(part.fParamIn[0].fC[9])>>myh","part.fStatusMaskIn[0].fData==31");
   isOK= abs(1-myh->GetRMS())<4*myh->GetRMSError();
    if (isOK) {::Info("testFastTracker pull test P3","pullAnalytical - OK");
   }else{::Error("testFastTracker pull test P3","pullAnalytical- FAILED");
   }
   
-  treeFast->Draw("(part.fParamIn[0].fP[2]-part.fParamMC[0].fP[2])/sqrt(part.fParamIn[0].fC[5])>>myh","part.fStatusMaskIn[0]==31");
+  treeFast->Draw("(part.fParamIn[0].fP[2]-part.fParamMC[0].fP[2])/sqrt(part.fParamIn[0].fC[5])>>myh","part.fStatusMaskIn[0].fData==31");
   isOK= abs(1-myh->GetRMS())<4*myh->GetRMSError();
    if (isOK) {::Info("testFastTracker pull test P2","pullAnalytical - OK");
   }else{::Error("testFastTracker pull test P2","pullAnalytical- FAILED");
   }
-  treeFast->Draw("(part.fParamIn[0].fP[1]-part.fParamMC[0].fP[1])/sqrt(part.fParamIn[0].fC[2])>>myh","part.fStatusMaskIn[0]==31");
+  treeFast->Draw("(part.fParamIn[0].fP[1]-part.fParamMC[0].fP[1])/sqrt(part.fParamIn[0].fC[2])>>myh","part.fStatusMaskIn[0].fData==31");
   isOK= abs(1-myh->GetRMS())<4*myh->GetRMSError();
    if (isOK) {::Info("testFastTracker pull test P1","pullAnalytical - OK");
   }else{::Error("testFastTracker pull test P1","pullAnalytical- FAILED");
   }
-  treeFast->Draw("(part.fParamIn[0].fP[0]-part.fParamMC[0].fP[0])/sqrt(part.fParamIn[0].fC[0])>>myh","part.fStatusMaskIn[0]==31");
+  treeFast->Draw("(part.fParamIn[0].fP[0]-part.fParamMC[0].fP[0])/sqrt(part.fParamIn[0].fC[0])>>myh","part.fStatusMaskIn[0].fData==31");
   isOK= abs(1-myh->GetRMS())<4*myh->GetRMSError();
    if (isOK) {::Info("testFastTracker pull test P0","pullAnalytical - OK");
   }else{::Error("testFastTracker pull test P0","pullAnalytical- FAILED");
